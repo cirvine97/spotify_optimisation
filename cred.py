4c5198d18ad57ddf8c53fba140c77aadc1cb59e0
Curtis Irvine <curtis.irvine@peak.ai>
2023-01-28 14:46:20 +0000

    deletion

--- cred.py
+++
@@ -1,5 +0,0 @@
-client_ID = '4a4e454ec1624dfebdf0d2565a75bb7b'
-client_SECRET = 'c393894f3f9646ad819d27a2ea8cc23b'
-redirect_url = 'http://localhost:9000'
-birthday_playlist_id ='77x1p7WNnpYs9do1TiVleR'
-test_id = '5rZ2fbCSIyU5cKiRcl7RtP'
