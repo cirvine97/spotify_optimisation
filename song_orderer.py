fe0dce2048d8a97374929c34ec494740faeaca92
Curtis Irvine <curtis.j.irvine@bbc.co.uk>
2024-02-29 14:46:49 +0000

    Write the playlist class for metadata

--- song_orderer.py
+++ song_orderer.py
@@ -1,43 +1,86 @@
 import pandas as pd
-import numpy as np
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import regex
-# Import label encoder
-from sklearn.preprocessing import LabelEncoder
-# Import credentials for spotify
+
 import cred
 
+# Instantiate the Spotipy object
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_ID,
+                                               client_secret=cred.client_SECRET,
+                                               redirect_uri=cred.redirect_url,
+                                               scope='playlist-read-private'))
+
 class Playlist:
+    """Retrieves and stores metadata of a Spotify playlist.
+    """
     def __init__(
-        self,
-        playlist_path: str,    
-    ) -> None:
-        self.path = playlist_path
-        self.playlist = pd.read_csv(self.path, on_bad_lines='skip')
-        
-    def generate_features(
-        self
-    ) -> None:
-        # Encode and normalise album date field
-        self.playlist = self.playlist.sort_values(
-            by=['Album Date'], 
-            ascending=False
-        )
-        self.playlist['album_date_encoded'] = LabelEncoder().fit_transform(self.playlist['Album Date'])
-        self.playlist['album_date_encoded'] = (self.playlist['album_date_encoded'] / 
-                                               self.playlist['album_date_encoded'].max() * 100)
-        
-        # Generate intuition parameter based on how early song was added to playlist
-        self.playlist['Intuition'] = self.playlist['#'] / self.playlist['#'].max() * 100
-        self.playlist['Intuition'] = self.playlist['Intuition'].map(lambda x: 100 - x)
-        
-        # Cleaning
-        self.playlist['Loudness'] = self.playlist['Loudness'].apply(lambda x: regex.sub(r'[-]|[db]', '', x))
-        # self.playlist['Loudness'] = self.playlist['Loudness'].astype(int)
-        # self.playlist['Loudness'] = self.playlist['Loudness']/self.playlist['Loudness'].max() * 100
-        # self.playlist['Loudness'] = self.playlist['Loudness'].map(lambda x: 100 - x)
-        
-        # self.playlist['Time'] = self.playlist['Time'].apply(
-        #     lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1])
-        #                                                     )
+            self, 
+            playlist_id
+        ):
+        self.playlist_id = playlist_id
+        self.playlist = sp.playlist_tracks(playlist_id)
+        self.playlist_metadata_df = self.get_playlist_data()
+    
+    def get_audio_features(
+            self, 
+            track_id
+        ):
+        audio_features = sp.audio_features(track_id)
+        return audio_features[0] if audio_features else {}
+    
+    def get_artist_genres(
+            self, 
+            artist_id
+        ):
+        artist_data = sp.artist(artist_id)
+        return artist_data['genres']
+    
+    def get_playlist_data(
+            self
+        ):
+        track_data = []
+
+        for item in self.playlist['items']:
+            track = item['track']
+            track_id = track['id']
+            audio_features = self.get_audio_features(track_id)
+
+            # Get genre info of artists
+            artist_ids = []
+            for artist in track['artists']:
+                artist_ids.append(artist['id'])
+            genres = []
+            for artist_id in artist_ids:
+                genres += self.get_artist_genres(artist_id)
+            # Remove duplicates
+            genres = set(genres)
+
+            track_info = {
+                'Track Name': track['name'],
+                'Artist Name': track['artists'][0]['name'],  # Assuming only one artist per track
+                'Album Name': track['album']['name'],
+                'Release Date': track['album']['release_date'],
+                'Duration (ms)': track['duration_ms'],
+                'Popularity': track['popularity'],
+                'Acousticness': audio_features.get('acousticness', None),
+                'Danceability': audio_features.get('danceability', None),
+                'Energy': audio_features.get('energy', None),
+                'Instrumentalness': audio_features.get('instrumentalness', None),
+                'Liveness': audio_features.get('liveness', None),
+                'Loudness': audio_features.get('loudness', None),
+                'Speechiness': audio_features.get('speechiness', None),
+                'Valence': audio_features.get('valence', None),
+                'Tempo': audio_features.get('tempo', None),
+                'Key': audio_features.get('key', None),
+                'Mode': audio_features.get('mode', None),
+                'Time Signature': audio_features.get('time_signature', None),
+                'Genres': genres,
+                'Top Genre': list(genres)[0] if genres else None
+            }
+
+            track_data.append(track_info)
+
+        playlist_df = pd.DataFrame(track_data)
+
+        return playlist_df
+    
