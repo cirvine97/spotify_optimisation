e6fd21dc3d88d41d14e0ab7553e58f142beb1e17
Curtis Irvine <curtis.j.irvine@bbc.co.uk>
2024-03-04 10:53:55 +0000

    Paginate and call API less

--- song_orderer.py
+++ song_orderer.py
@@ -36,48 +36,68 @@ class Playlist:
             raise ValueError("Playlist ID not found in the URL.")
 
 
-    def get_audio_features(
+    def get_audio_features_batch(
             self, 
-            track_id
+            track_ids
         ):
-        audio_features = sp.audio_features(track_id)
-        return audio_features[0] if audio_features else {}
+        audio_features_batch = []
+
+        # Split track IDs into batches of 100 (maximum limit per request)
+        for i in range(0, len(track_ids), 100):
+            track_ids_batch = track_ids[i:i+100]
+            audio_features_batch += sp.audio_features(track_ids_batch)
+
+        return audio_features_batch
 
     
-    def get_playlist_data(
-            self
-        ):
+    def get_playlist_data(self):
         track_data = []
 
-        for item in self.playlist['items']:
-            track = item['track']
-            track_id = track['id']
-            audio_features = self.get_audio_features(track_id)
+        # API will only return 100 songs at a time, need to paginate
+        offset = 0
+        limit = 100
 
-            track_info = {
-                'Track Name': track['name'],
-                'Artist Name': track['artists'][0]['name'],  # Assuming only one artist per track
-                'Album Name': track['album']['name'],
-                'Release Date': track['album']['release_date'],
-                'Duration (ms)': track['duration_ms'],
-                'Popularity': track['popularity'],
-                'Acousticness': audio_features.get('acousticness', None),
-                'Danceability': audio_features.get('danceability', None),
-                'Energy': audio_features.get('energy', None),
-                'Instrumentalness': audio_features.get('instrumentalness', None),
-                'Liveness': audio_features.get('liveness', None),
-                'Loudness': audio_features.get('loudness', None),
-                'Speechiness': audio_features.get('speechiness', None),
-                'Valence': audio_features.get('valence', None),
-                'Tempo': audio_features.get('tempo', None),
-                'Key': audio_features.get('key', None),
-                'Mode': audio_features.get('mode', None),
-                'Time Signature': audio_features.get('time_signature', None)
-            }
+        while True:
+            results = sp.playlist_tracks(self.playlist_id, offset=offset, limit=limit)
+            tracks = results['items']
 
-            track_data.append(track_info)
+            track_ids = [item['track']['id'] for item in tracks]
+            audio_features_batch = self.get_audio_features_batch(track_ids)
+
+            for i, item in enumerate(tracks):
+                track = item['track']
+                track_id = track['id']
+                audio_features = audio_features_batch[i]
+
+                track_info = {
+                    'Track Name': track['name'],
+                    'Artist Name': track['artists'][0]['name'],  # Assuming only one artist per track
+                    'Album Name': track['album']['name'],
+                    'Release Date': track['album']['release_date'],
+                    'Duration (ms)': track['duration_ms'],
+                    'Popularity': track['popularity'],
+                    'Acousticness': audio_features.get('acousticness', None),
+                    'Danceability': audio_features.get('danceability', None),
+                    'Energy': audio_features.get('energy', None),
+                    'Instrumentalness': audio_features.get('instrumentalness', None),
+                    'Liveness': audio_features.get('liveness', None),
+                    'Loudness': audio_features.get('loudness', None),
+                    'Speechiness': audio_features.get('speechiness', None),
+                    'Valence': audio_features.get('valence', None),
+                    'Tempo': audio_features.get('tempo', None),
+                    'Key': audio_features.get('key', None),
+                    'Mode': audio_features.get('mode', None),
+                    'Time Signature': audio_features.get('time_signature', None)
+                }
+
+                track_data.append(track_info)
+
+            if len(tracks) < limit:
+                break
+
+            offset += limit
 
         playlist_df = pd.DataFrame(track_data)
 
         return playlist_df
-    
+        
